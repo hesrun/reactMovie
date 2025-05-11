0e30b4d16de21b48d4079969744cffd40d7f355c
Vladyslav Otroshchenko <vladmih1988@gmail.com>
2025-05-11 14:51:07 +0200

    update

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="py-4">
-            <div className="container mx-auto">footer text</div>
-        </footer>
-    );
-};
-
-export default Footer;
